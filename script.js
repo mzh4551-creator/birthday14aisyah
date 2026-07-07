7783555a194789b9abbab6138005f12553739dcb
mzh4551-creator <mzh4551@gmail.com>
2026-07-07 16:06:46 +0700

    Implement click event for gift element
    Add click event to gift element for animation and display.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const gift = document.getElementById("gift");
+const surat = document.getElementById("surat");
 
+gift.addEventListener("click", function () {
+
+    gift.style.transform = "scale(1.3) rotate(15deg)";
+
+    setTimeout(() => {
+        gift.innerHTML = "🎉";
+        surat.classList.remove("hidden");
+    }, 500);
+
+});
